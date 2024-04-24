1a230f76d4dffbf5bdbef715fabf0890a88ce24e
LiamMcCausland <liammccausland65@gmail.com>
2024-04-24 14:14:23 -0500

    Resume Changes

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get the reference to the div element
+var div = document.getElementById('resume-button');
+
+// Add an event listener to handle the click event
+div.addEventListener('click', function () {
+    // Define the URL you want to open in a new tab
+    var url = 'assets/resume2024.pdf'; // Replace this with your actual URL
+
+    // Open the URL in a new tab
+    window.open(url);
+});
