50dd2396f06c3ba0375526cb594a79c937f8c707
LiamMcCausland <liammccausland65@gmail.com>
2024-05-07 23:06:15 -0500

    Resume Fix

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var div = document.getElementById('resume-button');
 // Add an event listener to handle the click event
 div.addEventListener('click', function () {
     // Define the URL you want to open in a new tab
-    var url = 'assets/resume2024.pdf'; // Replace this with your actual URL
-
+    var url = 'assets/resume2024.pdf';
     // Open the URL in a new tab
     window.open(url);
 });
